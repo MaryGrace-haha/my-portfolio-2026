b264aad926a5a08f4c9c59dc0c41c25da8c1a788
MaryGrace-haha <grasyagallito@gmail.com>
2026-05-31 21:29:45 +0800

    Update config and html

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/my-portfolio-2026/',
   plugins: [react()],
-  base: '/marygrace-haha.github.io/my-portfolio-2026/'
 })
