0f2394de5ea42806bbfef8860074b3fea02a4bfa
MaryGrace-haha <grasyagallito@gmail.com>
2026-05-31 21:12:43 +0800

    Fix base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// Palitan ang '/my-portfolio-2026/' ng pangalan ng repository mo sa GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio-2026/', 
+  base: '/marygrace-haha.github.io/my-portfolio-2026/'
 })
